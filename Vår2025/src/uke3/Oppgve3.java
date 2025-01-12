362c66112abb9f2032131a9bf207af8a35176cae
186480 <186480@stud.hvl.no>
2025-01-12 12:48:57 +0100

    op

---
+++ Vår2025/src/uke3/Oppgve3.java
@@ -0,0 +1,72 @@
+package uke3;
+
+import java.util.ArrayList;
+
+public class Oppgve3 {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> tall = new ArrayList<Integer>(); // Lager ny ArrayList object
+		
+		tall.add(1);
+		tall.add(2);
+		tall.add(4);
+		tall.add(4);
+		tall.add(4);
+		
+//		System.out.println(tall);
+//		System.out.println(tall.get(3));
+//		System.out.println(tall.get(4));
+		
+//		tall.add(1, 2);
+//		System.out.println(tall);
+//		
+//		System.out.println(finnes(tall, 2));
+//		System.out.println(finnes(tall, 5));
+		
+		like(tall, 10);
+		
+		System.out.println(tall.removeAll(tall));
+		System.out.println(tall);
+		
+		
+		
+		
+
+}
+	
+	public static void like(ArrayList<Integer> tabell, int tall) {
+		
+		for(int i = 0 ; i < tabell.size(); i++) {
+			if(tabell.get(i) == tall) {
+				System.out.println("Posisjon: " + i);
+			} 
+		}
+	}
+	
+	public static boolean finnes(ArrayList<Integer> tall, int innput) {
+		
+		for(int i = 0; i < tall.size(); i++) {
+			if(tall.get(i) == innput) {
+				return true;
+			}
+		} return false;
+		
+	}
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
